0ec18727b7a4aae22cd74bcdc8edec4c10c9793b
Praveen00912 <praveenkumarreddy2627@gmail.com>
2026-02-15 19:18:30 +0530

    added HelloWorld

---
+++ src/com/PraveenCodegnan/HelloWorldApp.java
@@ -0,0 +1,16 @@
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("==========================================");
+        System.out.println("    WELCOME TO JAVA PROGRAMMING          ");
+        System.out.println("==========================================");
+        System.out.println();
+        System.out.println("Hello, World!");
+        System.out.println("This is my first Java program.");
+        System.out.println("I am learning Java with Codegnan.");
+        System.out.println();
+        System.out.println("------------------------------------------");
+        System.out.println("Package: com.Praveencodegnan");
+        System.out.println("Version: 1.0");
+        System.out.println("==========================================");
+    }
+}
