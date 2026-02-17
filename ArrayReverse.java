9ece7a9aef8dfe52ceddc182fd7cef149164176c
Praveen00912 <praveenkumarreddy2627@gmail.com>
2026-02-17 21:58:36 +0530

    Added ArrayReverse

---
+++ ArrayReverse.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class ArrayReverse{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        reverseArray(arr);
+        System.out.println("Reversed array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        scanner.close();
+    }
+    private static void reverseArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
