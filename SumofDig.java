4f7c019cb5a1082f31783ba8c10979b3f708af42
Praveen00912 <praveenkumarreddy2627@gmail.com>
2026-03-01 17:57:22 +0530

    added Sum of Dig

---
+++ SumofDig.java
@@ -0,0 +1,12 @@
+class SumofDig {
+    public static void main(String[] args) {
+        int n=12354;
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+            sum=sum+d;
+            n=n/10;
+        }
+        System.out.println(sum);
+    }
+}
