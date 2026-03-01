f659e5ab1d37e04b25bcab3e962f5d537c872a95
Praveen00912 <praveenkumarreddy2627@gmail.com>
2026-03-01 18:02:50 +0530

    added Largest Number

---
+++ LargestNo.java
@@ -0,0 +1,13 @@
+class LargestNo {
+    public static void main(String[] args) {
+        int a=1000,b=100,c=90;
+        if(a>b && a>c){
+            System.out.println("a is greater");
+        }else if(b>a && b>c){
+            System.out.println("b is greater");
+            }
+            else{
+            System.out.println("c is greater");
+        }
+    }
+}
